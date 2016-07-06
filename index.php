b8bcb5990836e5546f2d0646687f5752a26546b6
dan <blankenshipdanielj@gmail.com>
2016-07-05 23:09:07 -0600

    second commit

---
+++ index.php
@@ -0,0 +1,159 @@
+<!doctype html>
+<html>
+<head>
+
+<!-- Include meta tag to ensure proper rendering and touch zooming -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Include jQuery Mobile stylesheets -->
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+
+<link rel="stylesheet" href="css/themes/ldsorg-theme.css" />
+<link rel="stylesheet" href="css/themes/jquery.mobile.icons.min.css" />
+<link rel="stylesheet" href="css/styles.css" />
+
+<!-- Include the jQuery library -->
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+<!-- Include the jQuery Mobile library -->
+<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+
+<script src="js/app.js"></script>
+
+</head>
+<body>
+    <div data-pagenum="1" class="page" data-role="page" id="pageone">
+      <div data-role="header">
+        <a href="#pageone" class="ui-btn-left ui-btn-corner-all ui-btn ui-icon-home ui-btn-icon-notext ui-shadow" title=" Home " data-form="ui-icon" data-role="button" role="button"> Home </a>
+        <h1 data-role="heading">Welcome To Page 1</h1>
+        <a class="ui-btn-right ui-btn-corner-all ui-btn ui-icon-grid ui-btn-icon-notext ui-shadow" title=" Navigation " data-form="ui-icon" data-role="button" role="button"> Navigation </a>
+      </div>
+      <div data-role="main" class="ui-content">
+        <h1>Missionary Digital Integrity</h1>
+        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+        <div class="navigation">
+          <a class="right" href="#pagetwo" data-transition="slide">Next Page</a>
+        </div>
+        <div class="currentPage">
+          <div class="pageTiles"></div>
+        </div>
+      </div>
+      <div data-role="footer">
+        <h1>lds.org</h1>
+      </div>
+    </div>
+
+    <div data-pagenum="2" class="page" data-role="page" id="pagetwo">
+      <div data-role="header">
+        <a href="#pageone" class="ui-btn-left ui-btn-corner-all ui-btn ui-icon-home ui-btn-icon-notext ui-shadow" title=" Home " data-form="ui-icon" data-role="button" role="button"> Home </a>
+        <h1 data-role="heading">Welcome To Page 2</h1>
+        <a class="ui-btn-right ui-btn-corner-all ui-btn ui-icon-grid ui-btn-icon-notext ui-shadow" title=" Navigation " data-form="ui-icon" data-role="button" role="button"> Navigation </a>
+      </div>
+      <div data-role="main" class="ui-content">
+        <h1>Choices</h1>
+        <div class="video-container">
+          <iframe id="choicesVideo" width="560" height="315" src="https://www.youtube.com/embed/RiI9aIFiF-k" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.</p>
+        <p>To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?</p>
+        <div class="navigation">
+          <a class="left" href="#pageone">Previous Page</a>
+          <a class="right" href="#pagethree" data-transition="slide">Next Page</a>
+        </div>
+        <div class="currentPage">
+          <div class="pageTiles"></div>
+        </div>
+      </div>
+      <div data-role="footer">
+        <h1>lds.org</h1>
+      </div>
+    </div>
+
+    <div data-pagenum="3" class="page" data-role="page" id="pagethree">
+      <div data-role="header">
+        <a href="#pageone" class="ui-btn-left ui-btn-corner-all ui-btn ui-icon-home ui-btn-icon-notext ui-shadow" title=" Home " data-form="ui-icon" data-role="button" role="button"> Home </a>
+        <h1 data-role="heading">Welcome To Page 3</h1>
+        <a class="ui-btn-right ui-btn-corner-all ui-btn ui-icon-grid ui-btn-icon-notext ui-shadow" title=" Navigation " data-form="ui-icon" data-role="button" role="button"> Navigation </a>
+      </div>
+      <div data-role="main" class="ui-content">
+        <h1>Honesty Pledge</h1>
+        <p>Do you pledge to not use missionary technology for anything other than furthering the work of the Lord?</p>
+        <h3>What does it mean to be honest?</h3>
+        <div data-role="collapsible-set" data-theme="c" data-content-theme="d">
+          <div data-role="collapsible">
+            <h3>Honesty as a virtue</h3>
+            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+          </div>
+          <div data-role="collapsible">
+            <h3>Honesty as a commitment</h3>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+          </div>
+          <div data-role="collapsible">
+            <h3>Honesty as a pursuit</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </div>
+        <label>
+          <input id="pledgeCheckbox" type="checkbox" name="pledgeCheckbox">I take the pledge
+        </label>
+        <a style="display: none" id="pledgeDialog" href="dialog.html" data-transition="slidedown">Link</a>
+        <div class="navigation">
+          <a class="left" href="#pagetwo">Previous Page</a>
+          <a id="pagethreeNext" class="right" href="#pagefour" data-transition="slide">Next Page</a>
+        </div>
+        <div class="currentPage">
+          <div class="pageTiles"></div>
+        </div>
+      </div>
+      <div data-role="footer">
+        <h1>lds.org</h1>
+      </div>
+    </div>
+
+    <div data-pagenum="4" class="page" data-role="page" id="pagefour">
+      <div data-role="header">
+        <a href="#pageone" class="ui-btn-left ui-btn-corner-all ui-btn ui-icon-home ui-btn-icon-notext ui-shadow" title=" Home " data-form="ui-icon" data-role="button" role="button"> Home </a>
+        <h1 data-role="heading">Welcome To Page 4</h1>
+        <a class="ui-btn-right ui-btn-corner-all ui-btn ui-icon-grid ui-btn-icon-notext ui-shadow" title=" Navigation " data-form="ui-icon" data-role="button" role="button"> Navigation </a>
+      </div>
+      <div data-role="main" class="ui-content">
+        <h1>Submit Your Name</h1>
+        <img class="photo" src="images/missionaries.jpg">
+        <label for="basic">Digital Signature</label>
+        <input type="text" name="name" id="digiSig" value=""  />
+        <div class="navigation">
+          <a class="left" href="#pagethree">Previous Page</a>
+          <a id="pagefourNext" class="right" href="#pagefive" data-transition="slide">Next Page</a>
+        </div>
+        <div class="currentPage">
+          <div class="pageTiles"></div>
+        </div>
+      </div>
+      <div data-role="footer">
+        <h1>lds.org</h1>
+      </div>
+    </div>
+
+    <div data-pagenum="5" class="page" data-role="page" id="pagefive">
+      <div data-role="header">
+        <a href="#pageone" class="ui-btn-left ui-btn-corner-all ui-btn ui-icon-home ui-btn-icon-notext ui-shadow" title=" Home " data-form="ui-icon" data-role="button" role="button"> Home </a>
+        <h1 data-role="heading">Welcome To Page 5</h1>
+        <a class="ui-btn-right ui-btn-corner-all ui-btn ui-icon-grid ui-btn-icon-notext ui-shadow" title=" Navigation " data-form="ui-icon" data-role="button" role="button"> Navigation </a>
+      </div>
+      <div data-role="main" class="ui-content">
+        <h1>Go Forth and Serve!</h1>
+        <p>You will be blessed for your diligent service.</p>
+        <img class="photo" src="images/jesus.jpg">
+        <div class="navigation">
+          <a class="left" href="#pagefour">Previous Page</a>
+        </div>
+        <div class="currentPage">
+          <div class="pageTiles"></div>
+        </div>
+      </div>
+      <div data-role="footer">
+        <h1>lds.org</h1>
+      </div>
+    </div>
+</body>
+</html>
---
+++ js/app.js
@@ -0,0 +1,60 @@
+function PagePosition(length, activePage) {
+  this.length = length;
+  this.activePage = activePage;
+}
+
+PagePosition.prototype.draw = function() {
+  var container = $('.pageTiles');
+  container.html('');
+
+  for (var i = 1; i <= this.length; i++) {
+    if (i == this.activePage) {
+      container.append("<div class='pageIndicator active'></div>")
+    } else {
+      container.append("<div class='pageIndicator'></div>");
+    }
+  }
+
+  container.css('width', $('.pageIndicator').outerWidth(true) * this.length);
+}
+
+function handleBreadcrumb() {
+  var pagesLength = $(".page").length;
+  var activePage = $.mobile.activePage.attr('data-pagenum');
+  var pagePos = new PagePosition(pagesLength, activePage);
+  pagePos.draw();
+}
+
+$(document).ready(function() {
+  handleBreadcrumb();
+  $(document).on("pagechange", function(event) {
+    handleBreadcrumb();
+  });
+
+  var player = $('#choicesVideo');
+  $('#pagetwo').on('pagebeforeshow', function() {
+    player.attr('src', 'https://www.youtube.com/embed/RiI9aIFiF-k');
+  });
+
+  $('#pagetwo').on('pagehide', function() {
+    player.attr('src', '');
+  });
+
+  $('#pagethreeNext').click(function(ev) {
+    var checkbox = $('#pledgeCheckbox');
+    if (!checkbox.is(':checked')) {
+      ev.preventDefault();
+      $('#pledgeDialog').click();
+    }
+  });
+
+  $('#pagefourNext').click(function(ev) {
+    var input = $('#digiSig');
+    if (input.val().length == 0) {
+      ev.preventDefault();
+      input.css('border', '1px solid red');
+    } else {
+      input.css('border', 'none');
+    }
+  });
+});
